a8afff19f60c8bd529c09ff2b564ae2e30d6d195
CptFacepalm <35321455+CptFacepalm@users.noreply.github.com>
2024-07-06 11:22:38 -0400

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+echo "Checking for updates"
+DOTNET_BUNDLE_EXTRACT_BASE_DIR="/home/container/lib" ./AlderonGamesCmd --game path-of-titans --server true --auth-token $AG_AUTH_TOKEN --beta-branch $BETA_BRANCH --install-dir .
+echo "Starting server"
+UE_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
+UE_PROJECT_ROOT=$(dirname "$UE_TRUE_SCRIPT_NAME")
+chmod +x "$UE_PROJECT_ROOT/PathOfTitans/Binaries/Linux/PathOfTitansServer-Linux-Shipping"
+"$UE_PROJECT_ROOT/PathOfTitans/Binaries/Linux/PathOfTitansServer-Linux-Shipping" PathOfTitans -log -Port=$SERVER_PORT -BranchKey=$BETA_BRANCH -AuthToken=$AG_AUTH_TOKEN -ServerGUID=$SERVER_GUID -Database=Local

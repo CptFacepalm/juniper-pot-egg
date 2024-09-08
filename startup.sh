12fd4a2413237aba75b257add68cc680a9400667
CptFacepalm <35321455+CptFacepalm@users.noreply.github.com>
2024-09-08 14:56:27 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,6 +3,8 @@ echo "Checking for updates"
 DOTNET_BUNDLE_EXTRACT_BASE_DIR="/home/container/lib" ./AlderonGamesCmd --game path-of-titans --server true --auth-token $AG_AUTH_TOKEN --beta-branch $BETA_BRANCH --install-dir .
 echo "Updating server roles"
 wget -O /mnt/server/PathOfTitans/Saved/Config/LinuxServer/Commands.ini https://raw.githubusercontent.com/CptFacepalm/juniper-pot-egg/main/Commands.ini
+echo "Waiting for 5 seconds to ensure update completes..."
+sleep 5
 echo "Starting server"
 UE_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
 UE_PROJECT_ROOT=$(dirname "$UE_TRUE_SCRIPT_NAME")
